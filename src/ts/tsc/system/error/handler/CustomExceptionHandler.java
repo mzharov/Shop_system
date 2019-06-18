a3a94ac94d1fc73a7ffbaf73d8e7bc292f549f2f
mzharov <zharmiha@yandex.ru>
2019-06-18 12:39:14 +0300

    Небольшие исправления

--- src/ts/tsc/system/error/handler/CustomExceptionHandler.java
+++ src/ts/tsc/system/error/handler/CustomExceptionHandler.java
@@ -2,45 +2,15 @@ package ts.tsc.system.error.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import ts.tsc.system.error.exception.NotFoundException;
-import ts.tsc.system.error.exception.ValidationException;
 import ts.tsc.system.error.frame.ErrorFrame;
-
-import javax.validation.ConstraintViolationException;
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorFrame> handleNotFoundException(NotFoundException exception) {
         return new ResponseEntity<>(exception.getErrorFrame(), HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<?> handleValidationExceprion(ValidationException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<?> handleConstraintValidationExceprion(ConstraintViolationException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getConstraintViolations().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
